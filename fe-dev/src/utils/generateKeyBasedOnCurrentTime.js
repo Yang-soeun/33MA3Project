f573d1ffc08e9b2059f0e04b1e721f58b6e00e75
Yang-soeun <soeun8636@naver.com>
2024-05-07 15:59:01 +0900

    [FEAT]: FE 폴더 삭제

--- fe-dev/src/utils/generateKeyBasedOnCurrentTime.js
+++
@@ -1,8 +0,0 @@
-// 날짜 기반 임의 키 생성 함수
-function generateKeyBasedOnCurrentTime() {
-  const currentTime = new Date().getTime();
-  const key = `key_${currentTime}`;
-  return key;
-}
-
-export default generateKeyBasedOnCurrentTime;
