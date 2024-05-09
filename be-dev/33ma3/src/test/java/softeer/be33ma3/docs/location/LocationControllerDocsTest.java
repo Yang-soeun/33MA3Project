30c3e2b1774d35696cea7677f64ee3a172a979d7
Yang-soeun <soeun8636@naver.com>
2024-05-09 18:48:27 +0900

    [FEAT]: 반경 안 센터 정보 내려주는 API Rest Docs 구현

--- be-dev/33ma3/src/test/java/softeer/be33ma3/docs/location/LocationControllerDocsTest.java
+++ be-dev/33ma3/src/test/java/softeer/be33ma3/docs/location/LocationControllerDocsTest.java
@@ -1,11 +1,14 @@
 package softeer.be33ma3.docs.location;
 
+import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.JsonFieldType.ARRAY;
 import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
 import static org.springframework.restdocs.payload.JsonFieldType.STRING;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -14,7 +17,6 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -57,5 +59,37 @@ public class LocationControllerDocsTest extends RestDocsSupport {
                                 fieldWithPath("data[].latitude").type(NUMBER).description("위도"),
                                 fieldWithPath("data[].longitude").type(NUMBER).description("경도")
                         )));
+    }
+
+    @DisplayName("반경 안 센터 정보를 내려주는 API")
+    @Test
+    void getCentersInRadius() throws Exception {
+        //given
+        CenterListDto center1 = new CenterListDto(1L, 35.4, 127.0);
+        List<CenterListDto> result =  List.of(center1);
+        given(locationService.getAllCenters()).willReturn(result);
+        given(locationService.getCentersInRadius(anyDouble(), anyDouble(), anyDouble())).willReturn(result);
+
+        //when //then
+        mockMvc.perform(get("/location?latitude=" + anyDouble() + "&longitude=" + anyDouble() + "&radius=" + anyDouble()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("SUCCESS"))
+                .andExpect(jsonPath("$.message").value("반경 내 위치한 센터 정보 전송 완료"))
+                .andExpect(jsonPath("$.data").isArray())
+                .andDo(document("inRadius-centers",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        queryParameters(parameterWithName("latitude").description("위도"),
+                                parameterWithName("longitude").description("경도"),
+                                parameterWithName("radius").description("반지름")),
+                        responseFields(
+                                fieldWithPath("status").type(STRING).description("상태"),
+                                fieldWithPath("message").type(STRING).description("성공 메세지"),
+                                fieldWithPath("data").type(ARRAY).description("데이터"),
+                                fieldWithPath("data[].centerId").type(NUMBER).description("센터 아이디"),
+                                fieldWithPath("data[].latitude").type(NUMBER).description("위도"),
+                                fieldWithPath("data[].longitude").type(NUMBER).description("경도")
+                        )));;
     }
 }
