665c53aa42565c69e4c8c200c56d6004b7bdaa42
Yang-soeun <soeun8636@naver.com>
2024-05-16 17:13:29 +0900

    [TEST]: ImageService - saveImages 메소드 테스트 코드 작성

--- be-dev/33ma3/src/test/java/softeer/be33ma3/service/ImageServiceTest.java
+++ be-dev/33ma3/src/test/java/softeer/be33ma3/service/ImageServiceTest.java
@@ -1,45 +1,77 @@
 package softeer.be33ma3.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import org.assertj.core.api.Assertions;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import softeer.be33ma3.domain.Image;
+import softeer.be33ma3.repository.ImageRepository;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Transactional
 class ImageServiceTest {
+    private final static String FILE_NAME = "fileName";
+    private final static String FILE_URL = "fileUrl.png";
+
     @Autowired private ImageService imageService;
+    @Autowired private ImageRepository imageRepository;
     @MockBean private S3Service s3Service;
 
     @DisplayName("1개의 이미지를 저장할 수 있다.")
     @Test
     void saveImage() throws IOException {
         //given
-        String fileName = "fileName";
-        String fileUrl = "fileUrl.png";
         MockMultipartFile image = createImages();
 
-        given(s3Service.uploadFile(any(MultipartFile.class))).willReturn(fileName);
-        given(s3Service.getFileUrl(anyString())).willReturn(fileUrl);
+        given(s3Service.uploadFile(any(MultipartFile.class))).willReturn(FILE_NAME);
+        given(s3Service.getFileUrl(anyString())).willReturn(FILE_URL);
 
         //when
         Image savedImage = imageService.saveImage(image);
 
         //then
-        Assertions.assertThat(savedImage)
+        assertThat(savedImage)
                 .extracting("link", "fileName")
                 .containsExactly("fileUrl.png", "fileName");
     }
+
+    @DisplayName("여러개의 이미지를 저장할 수 있다.")
+    @Test
+    void saveImages() throws IOException {
+        //given
+        List<MultipartFile> multipartFiles = new ArrayList<>();
+        multipartFiles.add(createImages());
+        multipartFiles.add(createImages());
+
+        given(s3Service.uploadFile(any(MultipartFile.class))).willReturn(FILE_NAME);
+        given(s3Service.getFileUrl(anyString())).willReturn(FILE_URL);
+
+        //when
+        List<Image> images = imageService.saveImages(multipartFiles);
+
+        //then
+        verify(s3Service, times(2)).uploadFile(any(MultipartFile.class));
+        verify(s3Service, times(2)).getFileUrl(anyString());
+        assertThat(images).hasSize(2);
+    }
+
     private MockMultipartFile createImages() throws IOException {
         String fileName = "testImage"; //파일명
         String contentType = "jpg"; //파일타입
